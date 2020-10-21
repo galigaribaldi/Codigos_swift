3e80f913d6ebdd215415ef3421534e6222650ede
galigaribaldi <galigaribaldi@live.com>
2020-10-21 14:27:14 -0500

    Core Data con Swift

--- Projects/guardaPalabras/guardaPalabras/TableViewController.swift
+++ Projects/guardaPalabras/guardaPalabras/TableViewController.swift
@@ -16,6 +16,18 @@ class TableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+//        A
+        let appDelegate = UIApplication.shared.delegate as? AppDelegate
+        
+        let managedContext = appDelegate!.persistentContainer.viewContext
+//        B Fech = Ir a buscar y extraer
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Lista")
+//        C
+        do {
+            managedObjects = try managedContext.fetch(fetchRequest)
+        } catch let error as NSError {
+            print("No se pudo recueprar los datos, error: \(error), \(error.userInfo)")
+        }
 
     }
 
@@ -28,15 +40,18 @@ class TableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return palabrasTabla.count
+        return managedObjects.count
     }
 
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         
+        let managedObject = managedObjects[indexPath.row]
+        
 //        Por cada una de las filas que tenga nuestra tabla
-        cell.textLabel?.text = palabrasTabla[indexPath.row]
+        cell.textLabel?.text = managedObject.value(forKey: "palabra") as?
+        String
         
         return cell
     }
@@ -47,7 +62,7 @@ class TableViewController: UITableViewController {
         let guardar = UIAlertAction(title: "Agregar", style: .default, handler: { (UIAlertAction) -> Void in
             
             let textField = alerta.textFields!.first
-            self.palabrasTabla.append(textField!.text!)
+            self.guardaPalabra(palabra: textField!.text!)
 //            Refrescar los cambios
             self.tableView.reloadData()
             
@@ -58,6 +73,31 @@ class TableViewController: UITableViewController {
         alerta.addAction(guardar)
         alerta.addAction(cancelar)
         present(alerta, animated: true, completion: nil)
+        }
+    
+    func guardaPalabra(palabra: String) {
+        
+//        managedObjectContext objetos NS Managed Object
+        let appDelegate = UIApplication.shared.delegate as? AppDelegate
+        let managedContext = appDelegate!.persistentContainer.viewContext
+        
+//        NSManagedBobject
+        let entity = NSEntityDescription.entity(forEntityName: "Lista", in: managedContext)!
+        let managedObject = NSManagedObject(entity: entity, insertInto: managedContext)
+        
+//          Añadimos valores a las propiedades de dicho objeto
+        managedObject.setValue(palabra, forKeyPath: "palabra")
+        
+        
+//        Guardar los datos a disco
+        
+        do {
+            try managedContext.save()
+            managedObjects.append(managedObject)
+            
+        } catch let error as NSError {
+            print("No se pudo guardar, error: \(error), \(error.userInfo)")
+        }
         }
     }
     
