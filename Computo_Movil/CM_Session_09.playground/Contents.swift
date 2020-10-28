74c7550299cd373518316b4796977fd6e61904e6
galigaribaldi <galigaribaldi@live.com>
2020-10-28 13:47:57 -0600

    Sesion 09

---
+++ Computo_Movil/CM_Session_09.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+struct Empleado{
+  var name: String
+  var age: Int
+  var salary: Double
+
+  mutating func modifySalary(value: Double){
+    self.salary = self.salary + value
+  }
+
+  func suma(a: inout Int){
+    a = a + 10
+  }
+}
+
+var godin = Empleado(name: "Juancho", age: 34, salary: 265.50)
+godin.modifySalary(value: 200)
+print(godin.salary)
+
+var a = 10
+godin.suma(a: &a)
+print(a)
